371d50da9f7704695c277047df7e72f7e4d9c4a9
ji <alslahdk@naver.com>
2020-03-25 01:08:39 +0900

    first commit

---
+++ inha/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\alslahdk.desktop-h3vc5d2\onedrive\desktop\coronamap_deploy-master\coronamap_deploy-master\inha\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
